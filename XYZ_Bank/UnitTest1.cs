f4d92e1660970a49d0216ebc13ab5c01bcf4caad
mdmostafamunna <mdmostafamunna.sqa@gmail.com>
2023-04-10 12:35:50 +0600

    Add project files.

---
+++ XYZ_Bank/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace XYZ_Bank
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
